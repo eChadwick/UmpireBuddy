f3ff529eda469da8f3ce9c702d015d93ec4460c5
eChadwick <en.chadwick@gmail.com>
2019-02-05 22:24:54 -0600

    remove default action bar

--- app/src/main/java/com/example/umpirebuddy/MainActivity.java
+++ app/src/main/java/com/example/umpirebuddy/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import static com.example.umpirebuddy.R.id.toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
